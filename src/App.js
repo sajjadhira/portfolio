5caf872e02bcc2a3d791f5dc73a7eceda61e4a4c
sajjadhira <sajjad.hira12@gmail.com>
2024-01-22 15:18:48 +0600

    Some chaned added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2 className="text-xl font-bold pt-5">Hello world!</h2>
+      <h2 className="text-xl font-bold pt-5">Hello,</h2>
       <p className="text-3xl font-bold">This is Sajjad.</p>
       <p>I am goona tell about myself in this website.</p>
-      <p className="font-light">Stay Tight, its .</p>
-      <p className="font-bold">Thanks.</p>
+      <p className="font-bold pt-5">Thanks.</p>
     </div>
   );
 }
